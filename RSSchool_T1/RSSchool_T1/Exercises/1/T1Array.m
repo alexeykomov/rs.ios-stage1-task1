20be4d5af19e6287c347c67ff0b194bf40fdd60b
Aliaksei Komleu <aliaksei_komleu@epam.com>
2020-03-13 14:44:09 +0300

    feat: complete happy array task

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,39 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *input;
+    input = [[NSMutableArray alloc] initWithArray:sadArray copyItems:YES];
+    NSMutableArray *output;
+    output = [[NSMutableArray alloc] init];
+    while (true) {
+        NSLog(@"Pass");
+        for (int counter = 0; counter < [input count]; counter++) {
+            if (counter == 0 || counter == ([input count] - 1)) {
+                [output addObject: [input objectAtIndex:counter]];
+                continue;
+            }
+            int leftIndex = counter - 1;
+            int rightIndex = counter + 1;
+            int leftNeighbor = leftIndex >= 0 ?
+                [(NSNumber *)[input objectAtIndex: leftIndex] intValue] :
+                0;
+            int rightNeighbor = rightIndex < [sadArray count] ?
+                [(NSNumber *)[input objectAtIndex: rightIndex] intValue] :
+                0;
+            int current = [(NSNumber *)[input objectAtIndex:counter] intValue];
+            if ((leftNeighbor + rightNeighbor) > current) {
+                [output addObject: [input objectAtIndex:counter]];
+            }
+        }
+        if ([output count] == [input count]) {
+            break;
+        }
+        input = [[NSMutableArray alloc] initWithArray:output copyItems:YES];
+        [output removeAllObjects];
+    }
+    NSLog(@"Result array: %@", output);
+    NSLog(@"Count of output: %d", [output count]);
+    return output;
 }
 
 @end

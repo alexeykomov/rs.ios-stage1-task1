4d77840e058e815fcc05e48a49c266fc8692f7dd
Aliaksei Komleu <aliaksei_komleu@epam.com>
2020-03-13 16:20:22 +0300

    feat: complete min and max sum task

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -1,10 +1,30 @@
 #import "MiniMaxArrayConverter.h"
+#import "limits.h"
 
 @implementation MiniMaxArrayConverter
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    int minSum = INT_MAX;
+    int maxSum = INT_MIN;
+    for (int counterA = 0; counterA < [array count]; counterA++) {
+        int indexToSkip = counterA;
+        int sum = 0;
+        for (int counterB = 0; counterB < [array count]; counterB++) {
+            if (counterB == indexToSkip) {
+                continue;
+            }
+            sum += [(NSNumber *)[array objectAtIndex:counterB] intValue];
+        }
+        NSLog(@"Sum: %d", sum);
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+        if (sum < minSum) {
+            minSum = sum;
+        }
+    }
+    return @[@(minSum), @(maxSum)];
 }
 
 @end
